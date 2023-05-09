c6b220e2e3a1054e3d23673e30978fdbc659fab1
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2023-05-09 18:16:47 -0300

    adds range to linear equations

--- ValueObjects/LinearEquation.cs
+++ ValueObjects/LinearEquation.cs
@@ -1,8 +1,11 @@
 namespace ValueObjects;
+
 public class LinearEquation
 {
     public double Slope { get; set; }
     public double Intersection { get; set; }
-    // public Interval Interval1 { get; set; }
-    // public Interval Interval2 { get; set; }
+    public double X1 { get; set; }
+    public double Y1 { get; set; }
+    public double X2 { get; set; }
+    public double Y2 { get; set; }
 }
