22774b4882f06b63a49c843fdde99515fa27dc5b
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2023-05-09 18:17:31 -0300

    inserting interfaces methods

--- Calculation/IEquations.cs
+++ Calculation/IEquations.cs
@@ -1,4 +1,14 @@
-public interface IEquations
+using ValueObjects;
+
+namespace Calculation;
+
+interface IEquations
 {
-    public List<double> LinearEquation { get; set; }
+    public LinearEquation GetLinearEquation(Point P1, Point P2);
+    public double GetDistanceFromPointToLinearEquation(LinearEquation linearEquation, Point point);
+    public LinearEquation GetPerpendicularLinearEquation(Point point, double slope);
+    public Point GetIntersectionPointFromLinearEquations(LinearEquation linearEquation1, LinearEquation linearEquation2);
+    public double GetDistanceBetweenPoints(Point P1, Point P2);
+    public List<IntersectionPoint> GetValidIntersectionPointFromLinearEquationsAndPoint(List<LinearEquation> linearEquations, Point point, List<Point> polylinePoints, Point userPoint);
+    public IntersectionPoint GetDistanceFromPointToPolylinePoint(List<LinearEquation> linearEquations, Point point, List<Point> polylinePoints, Point userPoint, IntersectionPoint intersectionPoint);
 }
