b3058cbd9f4f6cd954df6cbdca962261ae863721
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2023-05-09 18:17:11 -0300

    creates IntersectionPoint

---
+++ ValueObjects/IntersectionPoint.cs
@@ -0,0 +1,8 @@
+namespace ValueObjects;
+
+public class IntersectionPoint
+{
+  public Point Point { get; set; }
+  public double Distance { get; set; }
+  public LinearEquation LinearEquation { get; set; }
+}
