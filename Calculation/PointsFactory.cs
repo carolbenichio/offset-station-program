b8d29ac08fd48138d11a8c58d09adce0811ebb9d
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2023-05-09 18:16:57 -0300

    corrects identation

--- Calculation/PointsFactory.cs
+++ Calculation/PointsFactory.cs
@@ -7,13 +7,11 @@ public class PointsFactory
     public Point Point { get; set; }
     public List<Point> PolylinePoints { get; set; } 
 
-    // public PointsFactory() { }
-
-      public Point PointGenerator(string point) // working
-      {
+    public Point PointGenerator(string point) // working
+    {
         Point P = new Point();
         P.X = double.Parse(point.Split(',')[0]); 
         P.Y = double.Parse(point.Split(',')[1]);
         return P;
-      }
+    }
 }
